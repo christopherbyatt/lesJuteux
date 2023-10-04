67a8bf5c9a4e3339706477b7690f60de112fde42
Christopher Byatt <2064371@csfoy.ca>
2023-10-04 14:19:15 -0400

    .

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -9,87 +10,207 @@
 </head>
 
 <body>
-<header class="header" role="banner">
-    <nav class="ctn_nav">
-    <img class="nav__logo" src="*" alt="La Pastèque">
-        <input type="text">
-        <ul class="nav">
-            <li class="nav__item">
-                <a href="*" class="nav__lien">Nouveautés</a>
-            </li>
-            <li class="nav__item">
-                <a href="*" class="nav__lien">Livres</a>
-            </li>
-            <li class="nav__item">
-                <a href="*" class="nav__lien">Artistes</a>
-            </li>
-            <li class="nav__item">
-                <a href="*" class="nav__lien">Galerie/Boutique</a>
-            </li>
-            <li class="nav__item">
-                <a href="*" class="nav__lien">À propos</a>
-            </li>
-            <li class="nav__item">
-                <a href="*" class="nav__lien">Production La Pastèque</a>
-            </li>
-            <li class="nav__item">
-                <a href="*" class="nav__lien">Contact</a>
-            </li>
+    <header class="header" role="banner">
+        <nav class="nav__ctn">
+        <div class="nav">
+            <div class="nav__largeur">
+                <a href="index.php" class="nav__titre">La Pasteque</a>
+                <div class="nav__recherche">
+                    <form action="">
+                        <input type="text" name="recherche" class="nav__barre">
+                    </form>
+                </div>
+                <div class="nav__icon">
+                    <p>Allo</p>
+                </div>
+            </div>
+        </div>
+        <div class="nav__vert">
+            <div class="nav__largeur-menu">
+            <ul class="nav__liste">
+                <li class="nav__item">
+                    <a href="*" class="nav__lien">Nouveautés</a>
+                </li>
+                <li class="nav__item">
+                    <a href="*" class="nav__lien">Livres</a>
+                </li>
+                <li class="nav__item">
+                    <a href="*" class="nav__lien">Artistes</a>
+                </li>
+                <li class="nav__item">
+                    <a href="*" class="nav__lien">Galerie/Boutique</a>
+                </li>
+                <li class="nav__item">
+                    <a href="*" class="nav__lien">À propos</a>
+                </li>
+                <li class="nav__item">
+                    <a href="*" class="nav__lien">Production La Pastèque</a>
+                </li>
+                <li class="nav__item">
+                    <a href="*" class="nav__lien">Contact</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+        </nav>
+    </header>
+    <main>
+        <h1>Accueil</h1>
+        <h2>Nouveauté</h2>
+        <div class="livres">
+            <div class="livres__fiche">
+                <img src="*" alt="Jane, le renard et moi">
+                <h3>Jane, le renard et moi</h3>
+                <p>Isabelle Arsenault</p>
+                <p>Fanny Britt</p>
+                <p class="livres__prix">26.95$</p>
+            </div>
+            <div class="livres__fiche">
+                <img src="*" alt="Sanni et Jonas - Une nuit d'hiver">
+                <h3>Sanni et Jonas - Une nuit d'hiver</h3>
+                <p>Mari Ahokoivu</p>
+                <p>Kalle Hakkola</p>
+                <p class="livres__prix">18.95$</p>
+            </div>
+            <div class="livres__fiche">
+                <img src="*" alt="L'oiseau de Colette">
+                <h3>L'oiseau de Colette</h3>
+                <p>Isabelle Arsenault</p>
+                <p class="livres__prix">18.95$</p>
+            </div>
+            <div class="livres__fiche">
+                <img src="*" alt="Louis parmi les spectres">
+                <h3>Louis parmi les spectres</h3>
+                <p>Isabelle Arsenault</p>
+                <p>Fanny Britt</p>
+                <p class="livres__prix">34.95$</p>
+            </div>
+        </div>
+        <a href="" class="livres__lien">Toutes les nouveautés</a>
+        <div>
+            <h2>À venir</h2>
+            <div class="livre">
+                <img src="*" alt="Louis parmi les spectres">
+                <h3>Louis parmi les spectres</h3>
+                <p>Isabelle Arsenault</p>
+                <p>Fanny Britt</p>
+                <h4>34.95$</h4>
+            </div>
+            <div class="livre">
+                <img src="*" alt="Louis parmi les spectres">
+                <h3>Louis parmi les spectres</h3>
+                <p>Isabelle Arsenault</p>
+                <p>Fanny Britt</p>
+                <h4>34.95$</h4>
+            </div>
+            <div class="livre">
+                <img src="*" alt="Louis parmi les spectres">
+                <h3>Louis parmi les spectres</h3>
+                <p>Isabelle Arsenault</p>
+                <p>Fanny Britt</p>
+                <h4>34.95$</h4>
+            </div>
+            <div class="livre">
+                <img src="*" alt="Louis parmi les spectres">
+                <h3>Louis parmi les spectres</h3>
+                <p>Isabelle Arsenault</p>
+                <p>Fanny Britt</p>
+                <h4>34.95$</h4>
+            </div>
+            <a href="">Toutes les prochaines sorties</a>
+        </div>
+        <div>
+            <h2>Actualités</h2>
+            <div>
+                <h3>Paul à la maison dans le Sélection officielle d'Angoulême!</h3>
+                <p>Paul à la maison se retrouve dans la Sélection officielle du prochain Festival d'Angoulême! Bravo
+                    Michel!</p>
+            </div>
+            <div>
+                <h3>Réouverture de la boutique et deux promotions!</h3>
+                <p>La boutique de la rue Laurier va rouvrir ses portes avec un horaire réduit pour le temps des Fêtes.
+                    Vous pourrez venir nous visiter en personne le jeudi de 10h à 17h, le vendredi de 12h à 19h, le
+                    samedi et le dimanche de 10h à 17h.
+                    Pour faciliter vos achats en ligne et pour pallier à l’absence du Salon du livre cette année, La
+                    Pastèque vous offre des livres dédicacés par vos auteurs préférés, exclusivement sur notre site
+                    internet. Dès le 16 novembre prochain, surveillez notre page boutique!
+                    Aussi, dès le 12 novembre prochain, recevez gratuitement une carte de Noël exclusive de Jacques
+                    Goldstyn à chaque achat effectué sur notre site internet.</p>
+                <a href="">Accéder à la galerie/boutique</a>
+            </div>
+            <div>
+                <h3>La grande métamorphose de Théo sélectionné à Angoulême!</h3>
+                <p>La grande métamorphose de Théo de Geoffrey Delinte et Marzena Sowa est sélectionné pour le Prix des
+                    écoles 2021! Félicitations à nos auteurs!
+                    Le Prix des Écoles - Ville d'Angoulême récompense un album élu par les enfants de quatre écoles
+                    d’Angoulême au sein d’une liste de cinq albums présélectionnés, choisis pour leur potentiel éducatif
+                    et leurs contenus pédagogiques.</p>
+                <a href="">Info sur le livre La grande métamorphose de Théo</a>
+            </div>
+            <a href="">Lire plus d'actualités</a>
+        </div>
+        <div>
+            <h2>Événements</h2>
+            <div>
+                <h3>Le 12 août, j'achète un livre québécois!</h3>
+                <p>Pour le 12 août prochain, La Pastèque sort ses premiers titres du 2e semestre 2019! Retrouvez <a
+                        href="">Quand je serai mort</a> de Réal Godbout et Laurent Chabin, <a href="">Les étoiles</a> de
+                    Jacques Goldstyn, <a href="">Objet perdu</a> et <a href="">Coup de vent</a> de Cécile Gariépy!
+                    Jacques Goldstyn sera présent à la Librairie Laliberté samedi le 10 août à 10h30 pour une animation
+                    jeunesse.
+                    Réal Gobbout et Laurent Chabin seront chez Planète BD samedi le 10 août à 14h pour un lancement/
+                    dédicace.
+                    Et Cécile Gariépy sera à la Librairie Bric à brac, dimanche le 11 août à 10h pour une heure du
+                    conte.
+                    Bonne journée du livre québécois! </p>
+            </div>
+            <div>
+                <h3>François Vigneault en France</h3>
+                <p>François Vigneault, l'illustrateur de <a href="">13e avenue</a> sera présent en France en juin! Venez
+                    le rencontrer!</p>
+            </div>
+            <a href="">Tout les futurs événements</a>
+        </div>
+    </main>
+    <footer class="footer" role="contentinfo">
+        <p class="footer__titre">LA PASTÈQUE</p>
+        <ul class="footer__nav">
+            <nav class="ctn_footer">
+                <li class="footer__item">
+                    <a href="*" class="footer__lien">Nouveautés</a>
+                </li>
+                <li class="footer__item">
+                    <a href="*" class="footer__lien">Livres</a>
+                </li>
+                <li class="footer__item">
+                    <a href="*" class="footer__lien">Artistes</a>
+                </li>
+                <li class="footer__item">
+                    <a href="*" class="footer__lien">Galerie/Boutique</a>
+                </li>
+                <li class="footer__item">
+                    <a href="*" class="footer__lien">À propos</a>
+                </li>
+                <li class="footer__item">
+                    <a href="*" class="footer__lien">Production La Pastèque</a>
+                </li>
+                <li class="footer__item">
+                    <a href="*" class="footer__lien">Contact</a>
+                </li>
+            </nav>
+            <nav class="footer__info">
+                <p class="footer__texte">La Pastèque – Éditeur<br><br>Notre bureau et notre boutique se situent au 102
+                    avenue Laurier Ouest Montréal, H2T 2N7<br><br>Métro: Station Laurier</p>
+                <p class="footer__texte">Heures d’ouverture de la galerie-boutique<br>Lundi : fermé<br>Mardi :
+                    fermé<br>Mercredi: 10h à 17h<br>Jeudi : 10h à 17h<br>Vendredi : 10h à 17h<br>Samedi : 10h à
+                    17h<br>Dimanche : fermé</p>
+                <p class="footer__texte">Nous joindre<br><a class="footer__lien-mail"
+                        href="">info@lapasteque.com</a><br><br>Fondateurs et comité de rédaction:<br>Martin Brault<br><a
+                        class="footer__lien-mail" href="">brault@lapasteque.com</a><br>(514)502-0836<br><br>Frédéric
+                    Gauthier<br><a class="footer__lien-mail">fred@lapasteque.com</a><br>(514)627-1585</p>
+            </nav>
         </ul>
-    </nav>
-</header>
-<main>
-    <h1 class="titre">Accueil</h1>
-    <div>
-        <h2 class="sous-titre">Nouveauté</h2>
-        <div>
-            <img src="*" alt="Jane, le renard et moi">
-            <h3>Jane, le renard et moi</h3>
-            <p>Isabelle Arsenault</p>
-            <p>Fanny Britt</p>
-            <h4>26.95$</h4>
-        </div>
-        <div>
-            <img src="*" alt="Sanni et Jonas - Une nuit d'hiver">
-            <h3>Sanni et Jonas - Une nuit d'hiver</h3>
-            <p>Mari Ahokoivu</p>
-            <p>Kalle Hakkola</p>
-            <h4>18.95$</h4>
-        </div>
-    </div>
-</main>
-<footer class="footer" role="contentinfo">
-    <p class="footer__titre">LA PASTÈQUE</p>
-    <ul class="footer__nav">
-        <nav class="ctn_footer">
-        <li class="footer__item">
-            <a href="*" class="footer__lien">Nouveautés</a>
-        </li>
-        <li class="footer__item">
-            <a href="*" class="footer__lien">Livres</a>
-        </li>
-        <li class="footer__item">
-            <a href="*" class="footer__lien">Artistes</a>
-        </li>
-        <li class="footer__item">
-            <a href="*" class="footer__lien">Galerie/Boutique</a>
-        </li>
-        <li class="footer__item">
-            <a href="*" class="footer__lien">À propos</a>
-        </li>
-        <li class="footer__item">
-            <a href="*" class="footer__lien">Production La Pastèque</a>
-        </li>
-        <li class="footer__item">
-            <a href="*" class="footer__lien">Contact</a>
-        </li>
-        </nav>
-        <nav class="footer__info">
-            <p class="footer__texte">La Pastèque – Éditeur<br><br>Notre bureau et notre boutique se situent au 102 avenue Laurier Ouest Montréal, H2T 2N7<br><br>Métro: Station Laurier</p>
-            <p class="footer__texte">Heures d’ouverture de la galerie-boutique<br>Lundi : fermé<br>Mardi : fermé<br>Mercredi: 10h à 17h<br>Jeudi : 10h à 17h<br>Vendredi  : 10h à 17h<br>Samedi  : 10h à 17h<br>Dimanche  : fermé</p>
-            <p class="footer__texte">Nous joindre<br><a class="footer__lien-mail" href="">info@lapasteque.com</a><br><br>Fondateurs et comité de rédaction:<br>Martin Brault<br><a class="footer__lien-mail"href="">brault@lapasteque.com</a><br>(514)502-0836<br><br>Frédéric Gauthier<br><a class="footer__lien-mail">fred@lapasteque.com</a><br>(514)627-1585</p>
-        </nav>
-    </ul>
-</footer>
+    </footer>
 </body>
+
 </html>
