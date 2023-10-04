7ddd00918e9ff0eff6d7f3a2d80b5bb6563b0031
Clodiane Charette <2164515@csfoy.ca>
2023-10-04 14:28:03 -0400

    Finition MVC 3

--- ressources/vues/nouveautes/index.blade.php
+++ ressources/vues/nouveautes/index.blade.php
@@ -1,10 +1,5 @@
 @extends('gabarit')
 
 @section('contenu')
-        <h3>Page index des participants</h3>
-        <ul>
-            @foreach($participants as $participant)
-                <li><a href="index.php?controleur=participant&action=fiche&idParticipant={{$participant->getId()}}">{{$participant->getId()}}. {{$participant->getPrenom()}} {{$participant->getNom()}}</a></li>
-            @endforeach
-        </ul>
+    <h3>Contenu: {{$message}}</h3>
 @endsection
