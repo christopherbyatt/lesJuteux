2c6c11967aff7db3e71066ee2958c31947dd68ca
Rosalie <rosalie.roy@optimasolutions.ca>
2023-10-18 10:35:37 -0400

    Livres triés par auteurs

--- ressources/vues/livres/index.blade.php
+++ ressources/vues/livres/index.blade.php
@@ -1,10 +1,10 @@
 @extends('gabarit')
 
 @section('contenu')
-        <h3>Page index des participants</h3>
+        <h3>Tous les livres</h3>
         <ul>
             @foreach($livres as $livre)
-                <li><a href="index.php?controleur=livre&action=fiche&idLivre={{$livre->getId()}}">{{$livre->getId()}}. {{$livre->getTitre()}}</a></li>
+                <li><a href="index.php?controleur=livre&action=fiche&idLivre={{$livre->getId()}}">{{$livre->getTitre()}}</a></li>
             @endforeach
         </ul>
 @endsection
