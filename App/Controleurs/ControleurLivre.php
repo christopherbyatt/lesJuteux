2b777c1a43cd84e3abb04c69537b777d87b3637d
Rosalie <rosalie.roy@optimasolutions.ca>
2023-10-23 14:09:51 -0400

    Pagination liste de livres

--- App/Controleurs/ControleurLivre.php
+++ App/Controleurs/ControleurLivre.php
@@ -5,10 +5,22 @@ use App\Modeles\Livre;
 
 class ControleurLivre {
     public function index():void {
-        $livres = Livre::trouverTout();
+//        $livres = Livre::trouverTout();
         $livresAVenirs = Livre::trouverParVenir();
         $livresNouveautes = Livre::trouverParNouveautes();
-        $tDonnees = array("message" => "Je suis la page Index auteurs...", "livres" => $livres ,"livresAVenirs" => $livresAVenirs, "livresNouveautes"=>$livresNouveautes);
+
+        $urlPagination = 'index.php?controleur=livre&action=index';
+
+        if(isset($_GET['page'])){
+            $page = $_GET['page'];
+        }else {
+            $page = 0;
+        }
+
+        $livres = Livre::paginer($page,12);
+        $nbrPages = ceil(livre::compter()/12 -1);
+
+        $tDonnees = array("message" => "Je suis la page Index auteurs...", "livres" => $livres ,"livresAVenirs" => $livresAVenirs, "livresNouveautes"=>$livresNouveautes, "numeroPage"=>$page, "urlPagination" => $urlPagination, "nombreTotalPages"=>$nbrPages);
         echo App::getBlade()->run("livres.index", $tDonnees);
     }
 
