36ae51ff298974aae7ca5386a5d85acaa1598009
Rosalie <rosalie.roy@optimasolutions.ca>
2023-11-13 15:01:13 -0500

    Ajout format choisi dans message panier

--- public/liaisons/js/confirmationPanier.js
+++ public/liaisons/js/confirmationPanier.js
@@ -2,6 +2,17 @@ let btnAjouter = document.getElementById("btnAjouter");
 let message = document.getElementById("pConfirmation");
 let quantite = document.getElementById("chiffre");
 
+let papier = document.getElementById("papier");
+let pdf = document.getElementById("pdf");
+
 btnAjouter.addEventListener("click", function(){
-    message.innerHTML = "Vous venez d'ajouter " + quantite.value + " livre(s) au panier!";
+    message.innerHTML = "Vous venez d'ajouter " + quantite.value + " livre(s)";
+
+    if(papier.classList.contains("format-selectionne")){
+        message.innerHTML += " en format papier"
+    } else if(pdf.classList.contains("format-selectionne")){
+        message.innerHTML += " en format PDF"
+    }
+
+    message.innerHTML += " au panier!";
 });
