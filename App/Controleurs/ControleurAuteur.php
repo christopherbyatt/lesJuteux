c1f76a1413cf7efb02cc66e97aa7fa503c7c6c32
ClodianeC <2164515@csfoy.ca>
2023-11-16 13:36:21 -0500

    mini correction

--- App/Controleurs/ControleurAuteur.php
+++ App/Controleurs/ControleurAuteur.php
@@ -6,17 +6,7 @@ use App\Modeles\Livre;
 
 class ControleurAuteur {
     public function index():void {
-        // Code pour trouver l'URL actuel de JavaTPoint
-        // https://www.javatpoint.com/how-to-get-current-page-url-in-php
-        if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
-            $urlPagination = "https://";
-        else
-            $urlPagination = "http://";
-        // Append the host(domain name, ip) to the URL.
-        $urlPagination.= $_SERVER['HTTP_HOST'];
-
-        // Append the requested resource location to the URL
-        $urlPagination.= $_SERVER['REQUEST_URI'];
+        $urlPagination= "index.php?controleur=auteur&action=index";
 
         if(isset($_GET['page'])){
             $page = $_GET['page'];
