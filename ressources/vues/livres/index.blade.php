89b017cfa39b981b917346b7297814b33fcc0de5
ClodianeC <2164515@csfoy.ca>
2023-11-16 15:23:51 -0500

    finition etiquettes

--- ressources/vues/livres/index.blade.php
+++ ressources/vues/livres/index.blade.php
@@ -17,9 +17,9 @@
                 <div class="livres__fiche mode-grille" id="livres__fiche">
                 <a href="index.php?controleur=livre&action=fiche&idLivre={{$livre->getId()}}">
                     @if ($livre->getDateQuebec() > date('Y-m-d', strtotime("-12 months")) && $livre->getDateQuebec() < date('Y-m-d', time()))
-                        <div class="livreNouveautesEtiquette">Nouveauté</div>
+                        <div class="livreNouveautesEtiquette"><span class="brilleEtiquette"></span>Nouveauté</div>
                     @elseif($livre->getDateQuebec() > date('Y-m-d', time()))
-                        <div class="livreAVenirEtiquette">À venir</div>
+                        <div class="livreAVenirEtiquette"><span class="brilleEtiquette"></span>À venir</div>
                     @else
                         <div class="etiquetteVidePourReplirLEspace"></div>
                     @endif
@@ -88,9 +88,9 @@
                 <div class="livres__fiche">
                     <a href="index.php?controleur=livre&action=fiche&idLivre={{$livreNouveaute->getId()}}">
                         @if ($livreNouveaute->getDateQuebec() > date('Y-m-d', strtotime("-12 months")) && $livreNouveaute->getDateQuebec() < date('Y-m-d', time()))
-                            <div class="livreNouveautesEtiquette">Nouveauté</div>
+                            <div class="livreNouveautesEtiquette"><span class="brilleEtiquette"></span>Nouveauté</div>
                         @elseif($livreNouveaute->getDateQuebec() > date('Y-m-d', time()))
-                            <div class="livreAVenirEtiquette">À venir</div>
+                            <div class="livreAVenirEtiquette"><span class="brilleEtiquette"></span>À venir</div>
                         @else
                             <div class="etiquetteVidePourReplirLEspace"></div>
                         @endif
@@ -115,9 +115,9 @@
                 <div class="livres__fiche">
                     <a href="index.php?controleur=livre&action=fiche&idLivre={{$livreAVenir->getId()}}">
                         @if ($livreAVenir->getDateQuebec() > date('Y-m-d', strtotime("-12 months")) && $livreAVenir->getDateQuebec() < date('Y-m-d', time()))
-                            <div class="livreNouveautesEtiquette">Nouveauté</div>
+                            <div class="livreNouveautesEtiquette"><span class="brilleEtiquette"></span>Nouveauté</div>
                         @elseif($livreAVenir->getDateQuebec() > date('Y-m-d', time()))
-                            <div class="livreAVenirEtiquette">À venir</div>
+                            <div class="livreAVenirEtiquette"><span class="brilleEtiquette"></span>À venir</div>
                         @else
                             <div class="etiquetteVidePourReplirLEspace"></div>
                         @endif
