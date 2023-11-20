6a5ba5ba72f2128513a661e31d969d6a6d9a5bc2
Christopher Byatt <2064371@csfoy.ca>
2023-11-20 14:34:54 -0500

    page panier

--- ressources/vues/panier/index.blade.php
+++ ressources/vues/panier/index.blade.php
@@ -1,32 +1,36 @@
 @extends('gabarit')
 @section('contenu')
     <h1>Mon Panier</h1>
-    <div class="panier">
-        <div>
-        <img src="liaisons/images/livres/978897770105_w150.jpg">
-        <p><a href="" class="livres__titre">Titre du livre ici</a></p>
-        <p>Nom des auteurs ici</p>
-        <p>11.11$</p>
-        <a href="">Retirer du panier</a>
+    <div class="panier background">
+        <div class="panier__info">
+        <img class="panier__image" src="liaisons/images/livres/978897770105_w150.jpg">
+            <div class="panier__texte">
+        <p class="panier__p"><a href="" class="livres__titre">Titre du livre ici</a></p>
+        <p class="panier__p">Nom des auteurs ici</p>
+        <p class="panier__p">11.11$</p>
+            </div>
         </div>
-        <div>
-            <label for="chiffre"></label>
+        <div class="panier__quantitePrix">
+            <label for="chiffre">Quantité : </label>
             <input type="number" id="chiffre" name="chiffre" value="1">
             <p>Total : 11.11$</p>
-        </div>
-    </div>
-    <div class="panier">
-        <div>
-            <img src="liaisons/images/livres/978897770105_w150.jpg">
-            <p><a href="" class="livres__titre">Titre du livre ici</a></p>
-            <p>Nom des auteurs ici</p>
-            <p>11.11$</p>
             <a href="">Retirer du panier</a>
         </div>
-        <div>
-            <label for="chiffre"></label>
+    </div>
+    <div class="panier background">
+        <div class="panier__info">
+            <img class="panier__image" src="liaisons/images/livres/978897770105_w150.jpg">
+            <div class="panier__texte">
+            <p class="panier__p"><a href="" class="livres__titre">Titre du livre ici</a></p>
+            <p class="panier__p">Nom des auteurs ici</p>
+            <p class="panier__p">11.11$</p>
+            </div>
+        </div>
+        <div class="panier__quantitePrix">
+            <label for="chiffre">Quantité : </label>
             <input type="number" id="chiffre" name="chiffre" value="1">
             <p>Total : 11.11$</p>
+            <a href="">Retirer du panier</a>
         </div>
     </div>
 @endsection
