300a2b7ff8b53765f48791a36c341a4351102792
Rosalie <rosalie.roy@optimasolutions.ca>
2023-11-29 09:39:05 -0500

    Modal fonctionnel (ajout au panier)

--- public/liaisons/js/confirmationPanier.js
+++ public/liaisons/js/confirmationPanier.js
@@ -1,27 +1,27 @@
-let btnAjouter = document.getElementById("btnAjouter");
-let message = document.getElementById("pConfirmation");
-let quantite = document.getElementById("chiffre");
-
-let papier = document.getElementById("papier");
-let pdf = document.getElementById("pdf");
-
-btnAjouter.addEventListener("click", function(){
-
-    if (quantite.value == 1){
-        message.innerHTML = "Vous venez d'ajouter " + quantite.value + " livre";
-    } else if (quantite.value > 1) {
-        message.innerHTML = "Vous venez d'ajouter " + quantite.value + " livres";
-    }
-
-    if(papier.classList.contains("format-selectionne")){
-        message.innerHTML += " en format papier"
-    } else if(pdf.classList.contains("format-selectionne")){
-        message.innerHTML += " en format PDF"
-    }
-
-    message.innerHTML += " au panier!";
-
-    if (quantite.value <= 0){
-        message.innerHTML = "Veuillez entrer une quantité valide...";
-    }
-});
+// let btnAjouter = document.getElementById("btnAjouter");
+// let message = document.getElementById("pConfirmation");
+// let quantite = document.getElementById("chiffre");
+//
+// let papier = document.getElementById("papier");
+// let pdf = document.getElementById("pdf");
+//
+// btnAjouter.addEventListener("click", function(){
+//
+//     if (quantite.value == 1){
+//         message.innerHTML = "Vous venez d'ajouter " + quantite.value + " livre";
+//     } else if (quantite.value > 1) {
+//         message.innerHTML = "Vous venez d'ajouter " + quantite.value + " livres";
+//     }
+//
+//     if(papier.classList.contains("format-selectionne")){
+//         message.innerHTML += " en format papier"
+//     } else if(pdf.classList.contains("format-selectionne")){
+//         message.innerHTML += " en format PDF"
+//     }
+//
+//     message.innerHTML += " au panier!";
+//
+//     if (quantite.value <= 0){
+//         message.innerHTML = "Veuillez entrer une quantité valide...";
+//     }
+// });
