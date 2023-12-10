316732c47cdbdf0f97d8a14a44f6317875a69449
ClodianeC <2164515@csfoy.ca>
2023-12-10 14:48:55 -0500

    footer mobile

--- ressources/vues/fragments/pieddepage.blade.php
+++ ressources/vues/fragments/pieddepage.blade.php
@@ -1,13 +1,15 @@
 @include("fragments.navigation")
-</div>
-    <div class="footer__info">
-        <div class="footer__info-1">
-        <p class="footer__texte"><h4>La Pastèque – Éditeur</h4><br>Notre bureau et notre boutique se situent au 102 avenue Laurier Ouest Montréal, H2T 2N7<br><br>Métro: Station Laurier</p>
-        </div>
-        <div class="footer__info-2">
-            <p class="footer__texte"><h4>Heures d’ouverture de la galerie-boutique</h4>Lundi : fermé<br>Mardi : fermé<br>Mercredi: 10h à 17h<br>Jeudi : 10h à 17h<br>Vendredi  : 10h à 17h<br>Samedi  : 10h à 17h<br>Dimanche  : fermé</p>
-        </div>
-        <div class="footer__info-3">
-            <p class="footer__texte"><h4>Nous joindre</h4><a class="footer__lien-mail" href="">info@lapasteque.com</a><br><br><h4>Fondateurs et comité de rédaction:</h4>Martin Brault<br><a class="footer__lien-mail"href="">brault@lapasteque.com</a><br>(514)502-0836<br><br>Frédéric Gauthier<br><a class="footer__lien-mail" href="">fred@lapasteque.com</a><br>(514)627-1585</p>
-        </div>
+<div class="footer__info">
+    <div class="footer__info-1">
+    <p class="footer__texte"><h4>La Pastèque – Éditeur</h4><br>Notre bureau et notre boutique se situent au 102 avenue Laurier Ouest Montréal, H2T 2N7<br><br>Métro: Station Laurier</p>
     </div>
+    <div class="footer__info-2">
+        <p class="footer__texte"><h4>Heures d’ouverture de la galerie-boutique</h4>Lundi : fermé<br>Mardi : fermé<br>Mercredi: 10h à 17h<br>Jeudi : 10h à 17h<br>Vendredi  : 10h à 17h<br>Samedi  : 10h à 17h<br>Dimanche  : fermé</p>
+    </div>
+    <div class="footer__info-3">
+        <p class="footer__texte"><h4>Nous joindre</h4><a class="footer__lien-mail" href="">info@lapasteque.com</a><br><br><h4>Fondateurs et comité de rédaction:</h4>Martin Brault<br><a class="footer__lien-mail"href="">brault@lapasteque.com</a><br>(514)502-0836<br><br>Frédéric Gauthier<br><a class="footer__lien-mail" href="">fred@lapasteque.com</a><br>(514)627-1585</p>
+    </div>
+    <p class="footer__info__credits">La Pastèque 2023 - Projet monté dans le cadre du cours RPNI 3<br>
+    Techniques d'intégration multimédia Cégep de Sainte-Foy<br>
+    Christopher Byatt, Clodiane Charette, Rosalie Roy</p>
+</div>
